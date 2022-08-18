b1c0bce33f2b51492a16dc37aee028373382a925
SeoHyeonMyeong <gusaud2199@naver.com>
2022-08-18 22:32:45 +0900

    Fix: 각종 버그 수정
    - manifest.json을 찾을 수 없는 버그 수정
    - reportWebVitals 삭제

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ["./src/**/*.{js,jsx, html}"],
   mode: "jit",
   theme: {
     extend: {
